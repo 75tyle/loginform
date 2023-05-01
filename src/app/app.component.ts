260cd2f5809afbdfe16f0506df5d5d99f9be6854
sanghpal <kamblesanghpal2003@gmail.com>
2023-05-01 18:34:25 +0530

    sdfjkl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Inject }  from '@angular/core';
+import { DOCUMENT } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+
+constructor(@Inject(DOCUMENT) document: Document) {
+
+}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
   title = 'loginform';
+  
 }
